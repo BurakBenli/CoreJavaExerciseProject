b0d71444f693ac9e1ae03a0bb9af10aaceebf4ff
BurakBenli <bbenli999@gmail.com>
2019-06-08 21:14:16 +0300

    "stringDemo and miniProjeAsalSayı"

--- src/javacore/exercise/lesson12/miniProjeAsalSayı.java
+++ src/javacore/exercise/lesson12/miniProjeAsalSayı.java
@@ -3,31 +3,35 @@ package javacore.exercise.lesson12;
 public class miniProjeAsalSayı {
     public static void main(String[] args) {
 
-        int number = 23;
+        int number = 3;
+
+        int remeinder = number % 2 ;
+
+    //    System.out.println(remeinder);
 
         boolean isPrime = true;
 
-        if (number==1){
-            System.out.println("Asal sayı değildir!!");
+        if (number == 1){
+            System.out.println("Sayı Asal Değildir !!");
+            return;
         }
 
         if (number<1){
-            System.out.println("Geçersiz Sayı!!");
+            System.out.println("Geçersiz Sayı !!");
         }
 
-        for (int i=2;i<number;i++){
-            if(number % i == 0){
+
+        for(int i=2;i<number;i++){
+            if(number % i  == 0){
                 isPrime = false;
             }
         }
 
-        if (isPrime){
+        if (isPrime == true){
             System.out.println("Sayı asaldır");
         }else{
-            System.out.println("Sayı asal değildir");
+            System.out.println("Sayı Asal Değildir");
         }
-
-
 
     }
 }
